9ca3899656222210c84b4b67922bb9abcdd7690d
Sig Nin <signin@email.com>
2019-11-21 10:41:59 -0800

    Oops, class -> className; remove UserServices call

--- src/containers/home.js
+++ src/containers/home.js
@@ -10,13 +10,15 @@ class Home extends Component {
   }
 
   render() {
-    //(new UserServices()).userLoginWithSpotify();
     return(
       <div className='home'>
         <div className="row mt-5">
           <div className="col-md-10 m-auto">
             <div className="card card-body">
-              <h1 class="text-center mb-3"><i class="fas fa-sign-in-alt"></i>   Home</h1>
+              <h1 className="text-center mb-3">
+                <i className="fas fa-sign-in-alt"></i>
+                  {"    Home"}
+                </h1>
               <p>Log in with Spotify to begin searching ...</p>
               <div className="button-row">
                 <a href="http://localhost:8081/users/spotify" className="btn btn-secondary">
