10286b9036730e6e5b61f9a2ffdf0dd7412fbbd1
Sig Nin <signin@email.com>
2019-12-05 16:46:23 -0800

    Add navigation buttons to search results

--- src/containers/results.js
+++ src/containers/results.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import SearchButtons from "../components/searchButtons";
 
 class Results extends Component {
+  constructor(props){
+    super(props);
+    this.onNewSearch = this.onNewSearch.bind(this);
+  };
+
+  onNewSearch() {
+    this.props.onChangeSearchResults(null);
+  }
+
   render() {
     return (
       <div className="container">
         <h3>Results</h3>
-        <p>user ID = {this.props.results.userId}</p>
+        <p>Search Term: {this.props.results.searchResults.search_term}</p>
         <p>number of results = {Object.keys(this.props.results.searchResults.spotifyResponse).length}</p>
+        <SearchButtons onNewSearch={this.onNewSearch} />
       </div>
     )
   }
