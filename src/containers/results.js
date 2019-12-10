d179ae0a0f8de084ac0724d5ff867b45d4a18cd6
Sig Nin <signin@email.com>
2019-12-10 13:25:26 -0800

    Remove excess edge spacing and count from results

--- src/containers/results.js
+++ src/containers/results.js
@@ -15,20 +15,13 @@ class Results extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row mt-5">
-          <div className="col-md-10 m-auto">
-            <div className="card card-body">
-              <h1 className="text-center mb-3">
-                <i className="fas fa-search"></i>
-                {"    Search Results"}
-              </h1>
-              <p>Search Term: {this.props.results.searchResults.search_term}</p>
-              <p>number of results = {Object.keys(this.props.results.searchResults.spotifyResponse).length}</p>
-              <SearchResults id={this.props.id} spotifyResponse={this.props.results.searchResults.spotifyResponse} />
-              <SearchButtons id={this.props.id} onNewSearch={this.onNewSearch} />
-            </div>
-          </div>
-        </div>
+        <h1 className="text-center mb-3">
+          <i className="fas fa-search"></i>
+          {"    Search Results"}
+        </h1>
+        <p><strong>Search Term:</strong> {this.props.results.searchResults.search_term}</p>
+        <SearchResults id={this.props.id} spotifyResponse={this.props.results.searchResults.spotifyResponse} />
+        <SearchButtons id={this.props.id} onNewSearch={this.onNewSearch} />
       </div>
     )
   }
